151c1655caece184ed609e52441a5e4d6d1c2f9a
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-09-15 11:17:12 -0500

    react_native

--- 02_hibrido/react_native/frontend01/App.tsx
+++ 02_hibrido/react_native/frontend01/App.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HomeScreen } from './src/presentation/views/home/Home';
 import { RegisterScreen } from './src/presentation/views/register/Register';
 
 export type RootStackParamList = {
+  HomeScreen: undefined;
   RegisterScreen: undefined;
 }
 
@@ -12,7 +14,11 @@ const Stack = createNativeStackNavigator <RootStackParamList> ();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>        
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+        />
         <Stack.Screen
           name="RegisterScreen"
           component={RegisterScreen}
