e9326e62a992670119109e1582fabf4823ae300f
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-09-15 06:56:36 -0500

    react_native

--- 02_hibrido/react_native/frontend/src/presentation/views/home/Home.tsx
+++ 02_hibrido/react_native/frontend/src/presentation/views/home/Home.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, TextInput, Button, ToastAndroid } from 'react-native';
+import React, { useState } from 'react';
+import styles from './Styles';
+import { StyleSheet, Text, View, Image, TextInput, ToastAndroid, TouchableOpacity } from 'react-native';
 import { RoundedButton } from '../../components/RoundedButton';
+import { CustomTextInput } from '../../components/CustomTextInput';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../../../../App';
 import { useNavigation } from '@react-navigation/native';
+import  useViewModel  from './ViewModel';
 
 export const HomeScreen = () => {
 
+  const {email, password, onChange} = useViewModel();
   const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
 
   return (
@@ -24,106 +28,36 @@ export const HomeScreen = () => {
         </View>
         <View style={styles.form}>
           <Text style={styles.formText}>INGRESAR</Text>
-          <View style={styles.formInput}>
-            <Image style={styles.formIcon}
-              source={require('../../../../assets/email.png')}
-            />
-            <TextInput
-              style={styles.formTextInput}
-              placeholder='Correo electrónico'
-              keyboardType='email-address'
-            />
-          </View>
-          <View style={styles.formInput}>
-            <Image style={styles.formIcon}
-              source={require('../../../../assets/password.png')}
-            />
-            <TextInput
-              style={styles.formTextInput}
-              placeholder='Contraseña'
-              keyboardType='default'
-              secureTextEntry={true}
-            />
-          </View>
+          <CustomTextInput
+            image = {require('../../../../assets/email.png')}
+            placeholder = 'Correo Electrónico'
+            keyboardType = 'email-address'
+            property = 'email'
+            onChangeText = { onChange }
+            value = {email}
+          />
+          <CustomTextInput
+            image = {require('../../../../assets/password.png')}
+            placeholder = 'Contraseña'
+            keyboardType = 'default'
+            property = 'password'
+            onChangeText = { onChange }
+            value = {password}
+            secureTextEntry = { true }
+          />
           <View style={{ marginTop: 30 }}>
-            <RoundedButton text='ENVIAR' onPress={() => ToastAndroid.show('¡HOLA!', ToastAndroid.SHORT)}/>
+            <RoundedButton text='ENVIAR' onPress={() => {
+              console.log('Email: ' + email);
+              console.log('Password: ' + password);
+            }}/>
           </View>
           <View style={styles.formRegister}>
             <Text>¿No tienes cuenta?</Text>
-            <Text style={styles.formRegisterText}>Regístrate</Text>
+            <TouchableOpacity onPress={ () => navigation.navigate('RegisterScreen') }>
+              <Text style={styles.formRegisterText}>Regístrate</Text>
+            </TouchableOpacity>
           </View>
         </View>
       </View>
-    );
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-  },
-  imageBackground: {
-    width: '100%',
-    height: '100%',
-    opacity: 0.7,
-    bottom: '30%',
-  },
-  logoContainer: {
-    position: 'absolute',
-    alignSelf: 'center',
-    top: '15%',
-  },
-  logoImage: {
-    width: 100,
-    height: 100,
-  },
-  logoText: {
-    color: 'white',
-    textAlign: 'center',
-    fontSize: 20,
-    marginTop: 10,
-    fontWeight: 'bold',
-  },
-  form: {
-    width: '100%',
-    height: '40%',
-    backgroundColor: 'white',
-    position: 'absolute',
-    bottom: 0,
-    borderTopLeftRadius: 40,
-    borderTopRightRadius: 40,
-    padding: 30,
-  },
-  formText: {
-    fontWeight: 'bold',
-    fontSize: 16,
-  },
-  formIcon: {
-    width: 25,
-    height: 25,
-    marginTop: 5,
-  },
-  formInput: {
-    flexDirection: 'row',
-    marginTop: 30,
-  },
-  formTextInput: {
-    flex: 1,
-      borderBottomWidth: 1,
-      borderBottomColor: '#AAAAAA',
-      marginLeft: 15,
-  },
-  formRegister: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginTop: 20,
-  },
-  formRegisterText: {
-    fontStyle: 'italic',
-    color: 'orange',
-    borderBottomWidth: 1,
-    borderBottomColor: 'orange',
-    fontWeight: 'bold',
-    marginLeft: 10,
-  },
-});
