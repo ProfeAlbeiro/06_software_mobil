f58edc3fced554c151de13fa3d0f445b07666e9a
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-09-20 16:36:19 -0500

    react_native

--- 02_hibrido/react_native/frontend/src/domain/repositories/AuthRepository.tsx
+++
@@ -1,7 +0,0 @@
-import { User } from "../entities/User";
-import { ResponseApiDelivery } from "../../data/sources/remote/models/ResponseApiDelivery";
-
-export interface AuthRepository {
-  login(email: string, password: string): Promise<ResponseApiDelivery>;
-  register(user: User): Promise<ResponseApiDelivery>;
-}
